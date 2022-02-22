bfc5d6b042cb96fd383e06ce8dc7ca31ac45aed7
korolevskiy08 <emkorolev12@gmail.com>
2022-02-22 16:02:00 +0300

    feat: add folder dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes from'./Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNjf-BXFLlCx8qX_844X5C1Vlwan7pA19qCw&usqp=CAU" />
             </div>
