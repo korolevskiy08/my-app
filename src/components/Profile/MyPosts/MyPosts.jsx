43a49b0d4b0120c3b59eaa9369307a7851a81fa2
korolevskiy08 <emkorolev12@gmail.com>
2022-02-22 14:53:26 +0300

    feat: add folder MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={classes.posts}>
-                <Post value='20' message='Hi, how are you?'/>
-                <Post value='15' message="It's my first post"/>               
+                <Post value='20' message='Hi, how are you?' />
+                <Post value='15' message="It's my first post" />
             </div>
         </div>
     )
