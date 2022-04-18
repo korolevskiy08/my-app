fe9ec00d018f573009e53f80d00e9ecaee958346
korolevskiy08 <emkorolev12@gmail.com>
2022-04-18 10:50:39 +0300

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import store from './redux/state';
+import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
+console.log(store.getState())
 let rerenderEntireTree = (state) => {
+  console.log(state)
   ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
-        <App state={state} dispatch={store.dispatch.bind(store)} store={store} selectPostToChange={store.selectPostToChange.bind(store)} />
+        <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
       </React.StrictMode>
     </BrowserRouter>,
     document.getElementById('root')
@@ -19,7 +20,12 @@ let rerenderEntireTree = (state) => {
 
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree)
+store.subscribe(() => {
+  let state = store.getState()
+  rerenderEntireTree(state)
+}
+
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
